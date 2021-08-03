b72395486322a0c0b74ef1e3a4143e25d8a0e3ca
arielZarate <arieltecnico@gmail.com>
2021-08-03 19:43:16 -0300

    modificando loginBasico

--- LoginBasico/src/app/components/home/home.service.ts
+++
@@ -1,9 +0,0 @@
-import { BaseService } from './../../services/base.service';
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class HomeService extends BaseService {
-  //constructor() { }
-}
