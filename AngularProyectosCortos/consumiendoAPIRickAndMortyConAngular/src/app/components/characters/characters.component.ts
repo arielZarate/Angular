796907425b93118f9ae0590d8522341dd73d3e07
arielZarate <arieltecnico@gmail.com>
2021-11-30 19:09:18 -0300

    reiniciando

---
+++ AngularProyectosCortos/consumiendoAPIRickAndMortyConAngular/src/app/components/characters/characters.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CharacterService } from '../../services/character.service';
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+  styleUrls: ['./characters.component.css'],
+})
+export class CharactersComponent implements OnInit {
+  constructor(private _service: CharacterService) {}
+  //los datos deben llegar en array para ser desglosados en un iterador en la vista con un *ngFor
+  //este array se llamara en el html
+  characters: any = [];
+
+  //Le saque el :void
+
+  //ngOnInit(): void {}
+
+  async ngOnInit() {
+    const data: any = await this._service.all();
+    console.log(data);
+    this.characters = data.results;
+  }
+}
